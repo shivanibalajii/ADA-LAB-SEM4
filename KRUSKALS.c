d1b9c0aa11349edca2d08f6864888c4b08bcb1b5
shivanibalajii <bshivani.cs24@bmsce.ac.in>
2026-05-26 18:04:23 +0530

    Create KRUSKALS.c

---
+++ KRUSKALS.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int parent[10];
+
+int find(int i)
+{
+    while(parent[i])
+    {
+        i = parent[i];
+    }
+    return i;
+}
+
+int uni(int i, int j)
+{
+    if(i != j)
+    {
+        parent[j] = i;
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    int n, i, j, min, a, b, u, v;
+    int total = 0, edges = 0;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int cost[10][10];
+
+    printf("Enter cost matrix:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+        }
+    }
+
+    printf("Edges in MST:\n");
+
+    while(edges < n - 1)
+    {
+        min = 999;
+
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < n; j++)
+            {
+                if(cost[i][j] < min)
+                {
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+
+        u = find(u);
+        v = find(v);
+
+        if(uni(u, v))
+        {
+            printf("%d - %d = %d\n", a, b, min);
+            total += min;
+            edges++;
+        }
+
+        cost[a][b] = cost[b][a] = 999;
+    }
+
+    printf("Minimum Cost = %d\n", total);
+
+    return 0;
+}
